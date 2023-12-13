664fa2432b1bdd2c226a608f57d305aa8d52da1c
imancrrzii <imancarrazi777@gmail.com>
2023-12-13 11:29:39 +0700

    Add news writer attribute to article table and make factory from 50 to 100 for dummy data

--- carval-be/database/migrations/2023_12_04_160130_create_articles_table.php
+++ carval-be/database/migrations/2023_12_04_160130_create_articles_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('slug')->unique();
+            $table->string('news_writer');
             $table->string('source');
             $table->date('source_date');
             $table->string('description');
