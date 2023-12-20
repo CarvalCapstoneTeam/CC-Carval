2f211b5f4c66d4bd5c8fdfa11c2b961a38604042
dzakyy04 <dzakylinggau@gmail.com>
2023-12-20 13:02:46 +0700

    Modify the code to make it more optimized

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import pickle
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 import nltk
+import asyncio
 
 app = Flask(__name__)
 
 nltk.download('wordnet')
+nltk.download('punkt')
 
 token = 'token.pickle'
 
@@ -15,52 +18,58 @@ with open(token, 'rb') as token_file:
     credentials = pickle.load(token_file)
 
 model = load_model('model.h5')
-
 lemmatizer = WordNetLemmatizer()
+
+def preprocess_text(text):
+    words_list = word_tokenize(text)
+    lemmas = [lemmatizer.lemmatize(word.lower(), pos='v') for word in words_list]
+    return ' '.join(lemmas)
+
+async def predict_async(data):
+    title = data.get('title', '')
+    location = data.get('location', '')
+    department = data.get('department')
+    salary_range = data.get('salary_range', '')
+    company_profile = data.get('company_profile', '')
+    description = data.get('description', '')
+    requirements = data.get('requirements', '')
+    benefits = data.get('benefits', '')
+    telecommuting = data.get('telecommuting', '')
+
+    text = f"{title} {location} {department} {salary_range} {company_profile} {description} {requirements} {benefits} {telecommuting}"
+
+    processed_text = preprocess_text(text)
+    train_wordemb2 = credentials.texts_to_sequences([processed_text])
+    train_wordemb2 = pad_sequences(train_wordemb2, padding='post', maxlen=100)
+
+    prediction = model.predict(train_wordemb2)
+    real_probability = round(prediction[0, 0] * 100, 1)
+    fake_probability = round(100 - real_probability, 1)
+
+    result = 'Fake' if fake_probability >= 50 else 'Real'
+
+    return {
+        'error': False,
+        'message': 'Data predicted successfully',
+        'predictionResult': {
+            'prediction': result,
+            'fake_probability': fake_probability,
+            'real_probability': real_probability
+        }
+    }
 
 @app.route('/', methods=['GET'])
 def welcome():
-    return "Hello World"
+    return "OK"
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json(force=True)
-
-        title = data.get('title', '')
-        location = data.get('location', '')
-        departement = data.get('departement')
-        salary_range = data.get('salary_range', '')
-        company_profile = data.get('company_profile', '')
-        description = data.get('description', '')
-        requirements = data.get('requirements', '')
-        benefits = data.get('benefits', '')
-        telecommuting = data.get('telecommuting', '')
-
-        text = f"{title} {location} {departement} {salary_range} {company_profile} {description} {requirements} {benefits} {telecommuting}"
-        
-        words_list = []
-        for i in text :
-            words_list.extend(text.split())
-
-        lemmas = [lemmatizer.lemmatize(word.lower(), pos='v') for word in words_list]
-        train_wordemb2 = credentials.texts_to_sequences([lemmas])
-        train_wordemb2 = pad_sequences(train_wordemb2, padding='post', maxlen=100)
-        
-        prediction = model.predict(train_wordemb2)
-        real_probability = round(prediction[0, 0] * 100, 1)
-        fake_probability = round(100 - real_probability, 1)
-
-        if fake_probability >= 50:
-            result = 'Fake'
-        else:
-            result = 'Real'
-
-        return jsonify({
-            'prediction': result,
-            'fake_probability': fake_probability,
-            'real_probability': real_probability
-        })
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        result = loop.run_until_complete(predict_async(data))
+        return jsonify(result)
     except Exception as e:
         error_message = str(e)
         return jsonify({
